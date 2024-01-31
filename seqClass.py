25b89b8d2da44de9c68db79b84e6e7c5b2c5bd18
Andrea Bultó Sánchez <andreab@MacBook-Pro-de-Andrea.local>
2024-01-31 18:09:17 +0100

    Revert "Version 2"
    This reverts commit 7dc81974d38103a64f4cae2c1f60349be13dc193.
    Help changed

--- seqClass.py
+++ seqClass.py
@@ -4,7 +4,7 @@ import sys, re
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "xxxquence")
+parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 
 if len(sys.argv) == 1:
     parser.print_help()
