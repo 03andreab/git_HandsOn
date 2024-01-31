34fc69c0a69a90aa4c23a06bb884d7d5a192a980
Andrea Bultó Sánchez <andreab@MacBook-Pro-de-Andrea.local>
2024-01-31 17:51:01 +0100

    Version 1

---
+++ seqClass.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys, re
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
+parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+args = parser.parse_args()
+
+if re.search('^[ACGTU]+$', args.seq):
+    if re.search('T', args.seq):
+        print ('The sequence is DNA')
+    elif re.search('U', args.seq):
+        print ('The sequence is RNA')
+    else:
+        print ('The sequence can be DNA or RNA')
+else:
+    print ('The sequence is not DNA nor RNA')
