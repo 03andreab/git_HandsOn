6437ea1c6b7a7677adbd1a20b02b4375f50a352f
Andrea Bultó Sánchez <andreab@MacBook-Pro-de-Andrea.local>
2024-01-31 18:25:32 +0100

    Motif yes

--- seqClass.py
+++ seqClass.py
@@ -29,6 +29,6 @@ if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("FOUND")
+        print("YES!")
     else:
         print("NOT FOUND")
